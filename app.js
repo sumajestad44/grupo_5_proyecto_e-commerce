df3194482f7ea736c70bd2380bd5ec2e6883b594
unknown <sumajcoronel2002@gmail.com>
2022-01-25 23:07:04 -0300

    node listo, servidor listo, carpetas correspondientes listas, archivo html y css listos

---
+++ app.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+
+const publicFolderPath = path.resolve(__dirname, "./public");
+app.use(express.static(publicFolderPath));
+
+app.listen(3000, () => console.log('Servidor corriendo en puerto 3000'));
+
+app.get('/', (req, res) => res.sendFile(path.resolve(__dirname, './views/home.html')));
