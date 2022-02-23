660c7ddc7810e3cd186d86eca2deb1e66acc3e4a
sumajestad44 <sumajcoronel2002@gmail.com>
2022-02-23 20:20:56 -0300

    agregando estructura del login

--- app.js
+++ app.js
@@ -8,3 +8,4 @@ app.use(express.static(publicFolderPath));
 app.listen(3000, () => console.log('Servidor corriendo en puerto 3000'));
 
 app.get('/', (req, res) => res.sendFile(path.resolve(__dirname, './views/home.html')));
+app.get('/login', (req, res) => res.sendFile(path.resolve(__dirname, './views/login.html')));
