371870f77880ad9a43ac0205fe6795e62d2c9207
sumajestad44 <sumajcoronel2002@gmail.com>
2022-05-09 21:24:08 -0300

    image

--- src/middlewares/validateRegisterMiddleware.js
+++ src/middlewares/validateRegisterMiddleware.js
@@ -7,15 +7,4 @@ module.exports = [
     body('last_name').notEmpty().withMessage('Ingrese su apellido'),
     body('email').notEmpty().withMessage('Ingrese su email').isEmail().withMessage('Ingrese un email válido'),
     body('password').isLength({ min: 8 }).withMessage('Ingrese una contraseña con un mínimo de 8 caracteres'),
-    /**body('image').custom((value, {req})) => {
-        let file = req.file;
-        let acceptedExtensions = ['.jpg', '.png', '.gif']
-
-        let fileExtension = path.extname(file.originalname);
-        if(!acceptedExtensions.includes(fileExtension)){
-            throw new Error(`Las extensiones de archivo permitidas son ${acceptedExtensions.join(', ')}`);
-        }
-        return true;
-    }
-    */
 ]
