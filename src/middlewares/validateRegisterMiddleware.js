9540eb14e077e57512c9a30192de51b82654aa61
sumajestad44 <sumajcoronel2002@gmail.com>
2022-06-25 23:04:53 -0300

    Se había borrado el validateRegisterMiddleware por error

---
+++ src/middlewares/validateRegisterMiddleware.js
@@ -0,0 +1,39 @@
+const { body } = require('express-validator');
+const path = require('path');
+
+
+module.exports = [
+	body('first_name').notEmpty().isLength({ min: 2 }).withMessage('Ingrese su nombre'),
+	body('last_name').notEmpty().isLength({ min: 2 }).withMessage('Ingrese su apellido'),
+	body('email').isEmail().withMessage('Ingrese un email válido'),
+	body('password').notEmpty().isLength({ min: 8 }).withMessage('Ingrese una contraseña'),
+	body('password_confirm').custom((value, { req }) => {
+		if (req.body.password != req.body.password_confirm) {
+			throw new Error('Las contraseñas no coinciden')
+		}
+		else {
+			return true;
+		}
+	}),
+	body('image').custom((value, { req }) => {
+		if (req.file == undefined) {
+			return true;
+		}
+		if (req.file) {
+			let file = req.file;
+			let acceptedExtensions = [".jpg", ".jpeg", ".png", ".gif"];
+			let fileExtension = path.extname(file.originalname);
+			if (!acceptedExtensions.includes(fileExtension)) {
+				throw new Error(
+					`Las extensiones de archivo permitidas son ${acceptedExtensions.join(
+						", "
+					)}`,
+				);
+			}
+		}
+	})
+
+]
+
+
+
